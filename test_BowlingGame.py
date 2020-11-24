2cfacc3aa401797c91227f248868e55b1021269f
Lyderies <Lyderies@hotmail.com>
2020-11-25 09:49:52 +1300

    Changes to file structuring for testing

--- test_BowlingGame.py
+++ test_BowlingGame.py
@@ -1,26 +1,6 @@
 import unittest
-class BowlingGame(object):
-    def __init__(self):
-        self.throws= [] # change throw to throws 
-        self.score= 0
+import BowlingGame
 
-    def throw(self,pins):
-        self.throw.append(pins)
-
-
-    def calculate_score(self):
-        ball = 0
-        for frames in range(10):
-            if self.throws[ball]==10:
-                self.score +=10 + self.throws[ball+1] + self.throws[ball +2]
-                ball += 1
-            elif self.throw[ball] + self.throws[ball+1] == 10:
-                self.score += 10 + self.throws [ball +2]
-                ball +=2
-            else:
-                self.score += self.throws[ball] + self.throw[ball + 1]
-                ball += 2
- 
 class BowlingGameTests(unittest.TestCase):
     def throw_many(self, game, number_of_times, pins):
         for _ in range(number_of_times):
